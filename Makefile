9bae59f0b84e2439a555a3cc75e138e8e506ad59
U-US010\nyoscst3 <nyoscst3@nyo3316c.us010.siemens.net>
2011-10-18 20:17:25 -0400

    Add Makefile and hello world.

---
+++ Makefile
@@ -0,0 +1,20 @@
+GOFMT=gofmt -s -spaces=true -tabindent=false -tabwidth=4
+
+GOFILES=\
+  hello_world.go\
+
+TARGET=hello_world
+
+ifeq ($(GOOS),windows)
+  TARGET=hello_world.exe
+endif
+  
+all:
+	8g ellipsoid/ellipsoid.go
+	8g hello_world.go && 8l -o $(TARGET) hello_world.8 && ./$(TARGET) 
+	@echo Expected result:
+	@echo Distance = 543044.190419953 Bearing = 137.50134015496275
+	@echo lat3 = -122.21438161492877 lon3 = 37.74631054036373
+
+format:
+	${GOFMT} -w ${GOFILES}
---
+++ hello_world.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+import "./ellipsoid"
+
+func main() {
+    lat1, lon1 := 37.619002, -122.374843 //SFO
+    lat2, lon2 := 33.942536, -118.408074 //LAX
+
+    // Create Ellipsoid object with WGS84-ellipsoid, 
+    // angle units are degrees, distance units are meter.
+    geo1 := ellipsoid.Init("WGS84", ellipsoid.Degrees, ellipsoid.Meter, ellipsoid.Longitude_is_symmetric, ellipsoid.Bearing_is_symmetric)
+
+    // Calculate the distance and bearing from SFO to LAX.
+    distance, bearing := geo1.To(lat1, lon1, lat2, lon2)
+    fmt.Printf("Distance = %v Bearing = %v\n", distance, bearing)
+
+    // Calculate where you are when going from SFO in 
+    // direction 45.0 deg. for 20000 meters.
+    lon3, lat3 := geo1.At(lat1, lon1, 20000.0, 45.0)
+    fmt.Printf("lat3 = %v lon3 = %v\n", lat3, lon3)
+}
