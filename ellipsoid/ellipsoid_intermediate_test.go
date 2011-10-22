ec3dded5b08071b6755465c81b4ec25ade55aa70
Stefan Schroeder <nyoscst3@nyo3316c.us010.siemens.net>
2011-10-21 21:18:25 -0400

    Update tests

--- ellipsoid/ellipsoid_intermediate_test.go
+++ ellipsoid/ellipsoid_intermediate_test.go
@@ -4,8 +4,6 @@ package main
 import "fmt"
 import "./ellipsoid"
 
-var pass_counter, fail int
-
 func main() {
 
 	geo1 := ellipsoid.Init("WGS84", ellipsoid.Degrees, ellipsoid.Meter, ellipsoid.Longitude_not_symmetric, ellipsoid.Bearing_not_symmetric)
@@ -17,10 +15,7 @@ func main() {
 	// The array v is used to store the coordinates for all intermediate
 	// points (including the start and the end point). 
 	_, _, v := geo1.Intermediate(lat1, lon1, lat2, lon2, number_of_hops)
-	fmt.Printf("-------------------------\n")
 	for i := 0; i < len(v)/2; i++ {
 		fmt.Printf("%v %v # %v\n", v[i*2], v[i*2+1], i)
 	}
-	fmt.Printf("-------------------------\n")
-
 }
