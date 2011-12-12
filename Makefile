e563b27f05a3e3b37f4d5ec78f16ba047692dd01
Stefan Schroeder <nyoscst3@nyo3316c.us010.siemens.net>
2011-12-11 20:35:13 -0500

    Added LLA ECEF conversion

--- Makefile
+++ Makefile
@@ -12,9 +12,13 @@ endif
 all:
 	8g ellipsoid/ellipsoid.go
 	8g hello_world.go && 8l -o $(TARGET) hello_world.8 && ./$(TARGET) 
-	@echo Expected result:
+	@echo Expected results:
 	@echo "  Distance = 543044.190419953 Bearing = 137.50134015496275"
 	@echo "  lat3 = 37.74631054036373 lon3 = -122.21438161492877"
+	@echo "  x = 1.1042590709397183e+06"
+	@echo "  y = -4.824765955871677e+06"
+	@echo "  z = 4.0093940281868847e+06"
+	@echo "  lat5 = 39.197807000000005 lon5 = -77.10857400000002 alt3 = 55.00000000093132"
 
 format:
 	${GOFMT} -w ${GOFILES}
--- hello_world.go
+++ hello_world.go
@@ -13,11 +13,30 @@ func main() {
 
     // Calculate the distance and bearing from SFO to LAX.
     distance, bearing := geo1.To(lat1, lon1, lat2, lon2)
+    fmt.Printf("----- Testing To\n");
     fmt.Printf("Distance = %v Bearing = %v\n", distance, bearing)
 
     // Calculate where you are when going from SFO in 
     // direction 45.0 deg. for 20000 meters.
+    fmt.Printf("----- Testing At\n");
     lat3, lon3 := geo1.At(lat1, lon1, 20000.0, 45.0)
     fmt.Printf("lat3 = %v lon3 = %v\n", lat3, lon3)
 
+    // Convert Lat-Lon-Alt to ECEF.
+    lat4, lon4, alt4 := 39.197807, -77.108574 , 55.0 // Some location near Baltimore
+    // that the author of the Perl module geo-ecef used. I reused the coords of the tests.
+    fmt.Printf("----- Testing ToECEF\n");
+    x, y, z := geo1.ToECEF(lat4, lon4, alt4)
+    fmt.Printf("x = %v \ny = %v \nz = %v\n", x, y, z)
+
+    // Convert ECEF to Lat-Lon-Alt.
+    x1, y1, z1 := 1.1042590709397183e+06, -4.824765955871677e+06, 4.0093940281868847e+06
+    fmt.Printf("----- Testing ToLLA\n");
+    lat5, lon5, alt5 := geo1.ToLLA(x1, y1, z1)
+    fmt.Printf("lat5 = %v lon5 = %v alt3 = %v\n", lat5, lon5, alt5)
+
+    fmt.Printf("----- Done ---\n");
 }
+
+
+
